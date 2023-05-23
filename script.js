747f964a645aaea1721d526ebfe90914a18be7ef
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-05-23 20:46:49 +0200

    Created the DOM selections, the operator function and the eventListener function related to the calculators numbers and operators.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+//  DOM selections
+const display_text = document.querySelector(".display-txt");
+const clear_btn = document.querySelector(".clear-btn");
+const delete_btn = document.querySelector(".delete-btn");
+const btns = document.querySelectorAll(".btns");
+
+const operator_function = function (value1, operator, value2) {
+  Number(value1);
+  Number(value2);
+  switch (operator) {
+    case "*":
+      return value1 * value2;
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "÷":
+      return value1 / value2;
+    default:
+      return "Something went wrong";
+  }
+};
+
+let math_operator;
+let second_nr;
+let first_nr;
+let controller = 1;
+btns.forEach((el) => {
+  el.addEventListener("click", function () {
+    if (el.hasAttribute("data-number") && controller === 1) {
+      if (typeof first_nr === "undefined") {
+        first_nr = el.textContent;
+      } else {
+        first_nr += el.textContent;
+      }
+
+      display_text.textContent = first_nr;
+    }
+  });
+});
