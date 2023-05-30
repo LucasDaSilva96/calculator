f812d2bd3aa9b42be4b7e7120a81b5ddc2180fe1
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-05-30 21:02:07 +0200

    Create the eventListener function on the keyboard, this function is almost the same as the eventListener on the calculators. Create the cleaner_display_function which replaces the "*" with "x" and the "/" with "÷", this is necessary in order for the operation_function to work. Create the isLetter function which checks if the user tries to enter a letter to the calculation.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-"use strict";
 //  DOM selections
 const display_text = document.querySelector(".display-txt");
 const clear_btn = document.querySelector(".clear-btn");
@@ -8,7 +7,7 @@ const btns = document.querySelectorAll(".btns");
 // This function calculates the values and return the result
 const operator_function = function (expression) {
   const operators = ["x", "÷", "+", "-"];
-
+  console.log(expression);
   // Remove any spaces from the expression
   const cleanedExpression = expression.replace(/\s/g, "");
   // Replace comma with dot for decimal values
@@ -174,3 +173,147 @@ delete_btn.addEventListener("click", function () {
   function_called = false;
   display_array = display_text.textContent;
 });
+
+// Function for the keyboard keys
+window.addEventListener("keydown", function (e) {
+  if (isLetter(e.key)) return;
+  let modified_key;
+  let operators = ["*", "/", "x", "÷", "+", "-"];
+  // This is for converting the x -> * and / -> ÷
+  console.log(typeof e.key);
+  if (e.key === "*") {
+    modified_key = "x";
+    operators.push(modified_key);
+  } else if (e.key === "/") {
+    modified_key = "÷";
+    operators.push(modified_key);
+  }
+
+  // This is for making sure that the user can't type letters to the calculator
+  if (e.code !== "Space" && e.key !== "Shift") {
+    // This is for making sure that the user can't type more then one "."
+    if (e.key === ".") {
+      if (!floating_point || function_called) return;
+      floating_point = false;
+    }
+
+    // This checks if the user has a correct input to the calculator so that the calculator
+    // can make a correct mathematical calculation.
+    if (
+      (display_text.textContent === "0" && operators.includes(e.key)) ||
+      (e.key === "Enter" && display_text.textContent === "0")
+    ) {
+      return (display_text.textContent = "0");
+    }
+
+    // This makes sure that the user can't go outside of the calculators display
+    // with the numbers and operators.
+    if (display_text.textContent.length <= 14) {
+      if (!isNaN(e.key)) {
+        if (typeof first_nr === "undefined") {
+          first_nr = e.key;
+        } else {
+          first_nr += e.key;
+        }
+      }
+
+      if (operators.includes(e.key) && display_text.textContent != "0") {
+        if (e.key === "*") {
+          math_operator = "x";
+        } else if (e.key === "/") {
+          math_operator = "÷";
+        } else {
+          math_operator = e.key;
+        }
+        // This is for making sure that the user can't type more then one "."
+        floating_point = true;
+      }
+
+      if (display_text.textContent === "0" && e.key != ".") {
+        display_text.textContent = first_nr;
+      } else {
+        display_text.textContent += e.key;
+      }
+
+      if (e.key != "Enter") {
+        display_array = cleaner_display_function(display_text.textContent);
+      }
+
+      if (e.key === "Enter") {
+        display_text.textContent = operator_function(display_array);
+      } else {
+        function_called = false;
+      }
+
+      // If the amount of numbers is at 15, then the user should only be able
+      // to press all the buttons with no number.
+    } else if (
+      operators.includes(e.key) &&
+      display_text.textContent.length === 15
+    ) {
+      if (e.key === "*") {
+        math_operator = "x";
+      } else if (e.key === "/") {
+        math_operator = "÷";
+      } else {
+        math_operator = e.key;
+      }
+
+      display_text.textContent += e.key;
+
+      if (e.key != "Enter") {
+        display_array = cleaner_display_function(display_text.textContent);
+      }
+
+      if (e.key === "Enter") {
+        display_text.textContent = operator_function(display_array);
+      } else {
+        function_called = false;
+      }
+
+      // This makes sure the that user can't go outside of the calculators display after pressing one of the operators button.
+    } else if (
+      display_text.textContent.length >= 16 &&
+      display_text.textContent.length <= 17
+    ) {
+      display_text.textContent += e.key;
+
+      if (e.key != "Enter") {
+        display_array = cleaner_display_function(display_text.textContent);
+      }
+
+      if (e.key === "Enter") {
+        display_text.textContent = operator_function(display_array);
+      } else {
+        function_called = false;
+      }
+    }
+  }
+});
+
+// This function replaces the "/"" with "÷", and the ""* with the "x".
+// This is for the operator_function to work
+const cleaner_display_function = function (str) {
+  let new_str = str.split("");
+  let str_length = new_str.length;
+
+  const multiplication = "x";
+  const division = "÷";
+
+  for (let i = 0; i <= str_length; i++) {
+    if (new_str[i] === "*") {
+      new_str[i] = multiplication;
+    } else if (new_str[i] === "/") {
+      new_str[i] = division;
+    }
+  }
+
+  return new_str.join("");
+};
+
+// This function checks if the current display has letters in it
+const isLetter = function (str) {
+  const pattern = /[a-zA-Z]/;
+  if (str === "Enter") return false;
+  return pattern.test(str);
+};
